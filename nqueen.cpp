b4b01a132a57878a4fe93b86e6c821652b8dbeea
nekochen <neutrino0311@gmail.com>
2017-12-07 12:45:56 -0800

    change the way to parse command line argument (...maybe should move to argparse anyway...)

--- nqueen.cpp
+++ nqueen.cpp
@@ -3,11 +3,17 @@
 #include "LocalSearchTester.h"
 
 int main(int argc, const char *argv[]) {
-    int N = (argc > 1)? stoi(string(argv[1])) : 4;
-    int repeat = (argc > 2)? stoi(string(argv[2])) : 1;
-    int soft_threshold = (argc > 3)? stoi(string(argv[3])) : 2;
-    int prob_plateau = (argc > 4)? stoi(string(argv[4])) : 3;
-    int verbose = (argc > 5)? stoi(string(argv[5])) : 0;
+    int arg = 1;
+    int N = (argc > arg)? stoi(string(argv[arg])) : 4;
+    arg++;
+    int repeat = (argc > arg)? stoi(string(argv[arg])) : 1;
+    arg++;
+    int soft_threshold = (argc > arg)? stoi(string(argv[arg])) : 2;
+    arg++;
+    int prob_plateau = (argc > arg)? stoi(string(argv[arg])) : 3;
+    arg++;
+    int verbose = (argc > arg)? stoi(string(argv[arg])) : 0;
+    arg++;
 
     LocalSearchTester<LocalSearchRunner<NQueenSwapSolver>> tester(N, repeat, 
             soft_threshold, prob_plateau);
